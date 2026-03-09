9bb4794b04fc2638722bda4f838a1d7294d32873
Brett Knadle <brett182x@gmail.com>
2026-03-08 18:28:33 -0700

    Add welcome panel

---
+++ Assets/Scripts/UI/WelcomePanel.cs
@@ -0,0 +1,15 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
+using UnityEngine;
+
+public class WelcomePanel : MonoBehaviour
+{
+    [SerializeField] private GameObject welcomePanel;
+    [SerializeField] private TextMeshProUGUI revenueLabel;
+    [SerializeField] private Image backdrop;
+
+    private void OnEnable()
+    {
+
+    }
+}
